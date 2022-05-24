55ecec5ad295f9d6a6923202650a2d8ef06e486a
at2f <91329492+at2f@users.noreply.github.com>
2022-05-24 16:39:37 +0200

    Update installer_laurux.sh

--- installation_de_logiciels/installer_laurux.sh
+++ installation_de_logiciels/installer_laurux.sh
@@ -4,9 +4,17 @@ sudo snap refresh
 sudo snap install laurux || sudo snap install --candidate laurux
 sudo apt install -y mysql-server
 utilisateur=$USER
-echo
-echo "Créer un mot de passe pour le compte MySQL "${utilisateur}" :"
-sudo mysql --password --execute="CREATE USER "$utilisateur"@localhost;"
-echo
-echo "Entrer le mot de passe du compte MySQL "${utilisateur}" :"
-sudo mysql --password --execute="GRANT ALL PRIVILEGES ON *.* TO "${utilisateur}"@localhost;"
+read -rp "Créer un mot de passe pour le compte MySQL "${utilisateur}" :" mot_de_passe_msyql
+#echo
+#echo "Créer un mot de passe pour le compte MySQL "${utilisateur}" :"
+sudo mysql --password="${mot_de_passe_msyql}" --execute="CREATE USER "$utilisateur"@localhost;"
+#echo
+#echo "Entrer le mot de passe du compte MySQL "${utilisateur}" :"
+sudo mysql --password="${mot_de_passe_msyql}" --execute="GRANT ALL PRIVILEGES ON *.* TO "${utilisateur}"@localhost;"
+
+cat << EOF
+Se connecter à la base de donnée MySQL dans Laurux :
+Utilisateur : ${utilisateur}
+Mot de passe : ${mot_de_passe_msyql}
+@127.0.0.1
+EOF
