2a21f016d708e3d0095a217b153b1f9157548de2
at2f <91329492+at2f@users.noreply.github.com>
2022-05-24 16:35:25 +0200

    Update installer_laurux.sh

--- installation_de_logiciels/installer_laurux.sh
+++ installation_de_logiciels/installer_laurux.sh
@@ -5,7 +5,7 @@ sudo snap install laurux || sudo snap install --candidate laurux
 sudo apt install -y mysql-server
 utilisateur=$USER
 echo
-echo "Créer un mot de passe pour le compte MySQL "${utilisateur}" 0:"
+echo "Créer un mot de passe pour le compte MySQL "${utilisateur}" :"
 sudo mysql --password --execute="CREATE USER "$utilisateur"@localhost;"
 echo
 echo "Entrer le mot de passe du compte MySQL "${utilisateur}" :"
