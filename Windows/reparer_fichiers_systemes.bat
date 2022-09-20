23351e0cbeb4cfde6cb5bee514f2ff3ddcd1de2e
at2f <91329492+at2f@users.noreply.github.com>
2022-09-20 11:09:47 +0200

    Create reparer_fichiers_systemes.bat

---
+++ Windows/reparer_fichiers_systemes.bat
@@ -0,0 +1,9 @@
+@echo off
+
+:: Guide :
+:: https://www.windowscentral.com/how-use-dism-command-line-utility-repair-windows-10-image
+
+sfc /scannow
+DISM /Online /Cleanup-Image /CheckHealth
+DISM /Online /Cleanup-Image /ScanHealth
+DISM /Online /Cleanup-Image /RestoreHealth
